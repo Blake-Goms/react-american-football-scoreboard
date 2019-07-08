c4b59fd14487410fc148216531a1ec094f1399aa
Blake Goms <blakegoms@gmail.com>
2019-07-08 16:51:00 -0700

    MVP: Some Stretch

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+
 const BottomRow = () => {
+const [downCount, setDownCount] = useState(0)
+const [toGoCount, setToGoCount] = useState(0)
+const [BallOn, setBallOn] = useState(0)
+const [Quarter, setQuarter] = useState(0)
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value" onClick={() => {setDownCount( downCount < 4 ? downCount + 1  : 0)}} > {downCount} </div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value" onClick={() => {setToGoCount( toGoCount < 10 ? toGoCount + 1  : 0)}} > {toGoCount} </div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value" onClick={() => {setBallOn( BallOn < 99 ? BallOn + 1  : 0)}}> {BallOn} </div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value" onClick={() => {setQuarter( Quarter < 4 ? Quarter + 1  : 0)}}> {Quarter} </div>
       </div>
     </div>
   );
 };
-
 export default BottomRow;
